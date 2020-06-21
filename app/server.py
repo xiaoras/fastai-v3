205c5730470b6d4784d8c370511bcb3e47d6a72a
xiaoras <58347557+xiaoras@users.noreply.github.com>
2020-06-21 17:11:46 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.googleapis.com/drive/v3/files/1v9ASw8CwB9wvi8nzlzHQwsPIiYqjJZKY?alt=media&key=AIzaSyAbgeAxDdaQJC-eF_RlEfrACr4WsW8VuHg'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['batman', 'captain america', 'deadpool', 'hulk', 'iron man', 'spiderman', 'superman', 'wolverine', 'wonder woman']
+classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
