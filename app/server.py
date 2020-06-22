1149487efc9ffd4bbb2c84dcc2ad909da4b80443
xiaoras <58347557+xiaoras@users.noreply.github.com>
2020-06-21 22:18:47 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -62,7 +62,7 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    img = img.resize((8, 8))
+#     img = img.resize((8, 8))
     
     pred_class, pred_idx, outputs = learn.predict(img)
     
