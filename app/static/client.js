9797ed7f485c626f79216a05c6d009ffd3cda816
xiaoras <58347557+xiaoras@users.noreply.github.com>
2020-06-21 19:54:03 -0500

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -30,8 +30,8 @@ function analyze() {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
       el("result-label").innerHTML = `This is most likely ${response["result"]}`;
-//       el("plot-label").innerHTML = `<img src=\'data:image/png;base64,{}\'>.format(${response["plot"]})`;
-      el("plot-label").innerHTML = `<img src=${response["plot"]}>`;
+      el("plot-label").innerHTML = `<img src=\'data:image/png;base64,{}\'>.format(${response["plot"]})`;
+//       el("plot-label").innerHTML = `<img src=${response["plot"]}>`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
