9858f229c7b9d3ef8303c277f0cfe393b561f854
xiaoras <58347557+xiaoras@users.noreply.github.com>
2020-06-29 10:37:10 -0500

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -14,53 +14,15 @@ function showPicked(input) {
   reader.readAsDataURL(input.files[0]);
 }
 
-// function analyze() {
-//   var uploadFiles = el("file-input").files;
-//   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
-
-//   el("analyze-button").innerHTML = "Analyzing...";
-//   var xhr = new XMLHttpRequest();
-//   var loc = window.location;
-//   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
-//     true);
-//   xhr.onerror = function() {
-//     alert(xhr.responseText);
-//   };
-//   xhr.onload = function(e) {
-//     if (this.readyState === 4) {
-//       var response = JSON.parse(e.target.responseText);
-//       el("result-label").innerHTML = `This is most likely ${response["result"]}`;
-//       el("plot-label").innerHTML = `${response["plot"]}`;
-//     }
-//     el("analyze-button").innerHTML = "Analyze";
-//   };
-
-//   var fileData = new FormData();
-//   fileData.append("file", uploadFiles[0]);
-//   xhr.send(fileData);
-// }
-
 function analyze() {
-  var fileData = new FormData();
-
-  var uploadFiles = el("file-input").files;    //check for uploaded image
-  var fileName = el("upload-label").innerHTML; //check for chosen filename
-
-  if (uploadFiles.length === 1) {
-    fileData.append("file", uploadFiles[0]);
-  } else if (fileName !== "No file chosen") {
-    fileData.append("filename", fileName);
-  } else {
-    alert("Please select a file to analyze!");
-    return;
-  }
+  var uploadFiles = el("file-input").files;
+  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
   el("analyze-button").innerHTML = "Analyzing...";
-  el("result-label").innerHTML = "<em>Please be patient: this may take up to 20 seconds.</em>";
-  el("spinner").className = "";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
-  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`, true);
+  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
+    true);
   xhr.onerror = function() {
     alert(xhr.responseText);
   };
@@ -71,9 +33,9 @@ function analyze() {
       el("plot-label").innerHTML = `${response["plot"]}`;
     }
     el("analyze-button").innerHTML = "Analyze";
-    el("spinner").className = "no-display";
   };
 
+  var fileData = new FormData();
+  fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
-
